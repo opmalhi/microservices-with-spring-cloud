39dd8d98a887f11bcbe975a7edef59f565f02a74
Om Parkash <opmalhi@gmail.com>
2024-11-22 18:46:01 -0600

    configure JPA and h2 in memory database and initialized data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10001, 'USD', 'PKR', '277.80', '');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10002, 'EUR', 'PKR', '289.31', '');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10003, 'AUD', 'PKR', '180.50', '');
+
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+values (10004, 'INR', 'PKR', '3.29', '');
