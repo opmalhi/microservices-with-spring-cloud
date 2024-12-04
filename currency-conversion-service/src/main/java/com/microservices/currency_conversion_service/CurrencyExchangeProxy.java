80925806882d66e419c96909001f6b3630f56379
Om Parkash <opmalhi@gmail.com>
2024-12-04 15:12:09 -0600

    load balancing with eureka, feign and spring cloud loadBalancer

--- currency-conversion-service/src/main/java/com/microservices/currency_conversion_service/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/microservices/currency_conversion_service/CurrencyExchangeProxy.java
@@ -5,7 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //we need to use application name of the service as a FeignClient name which we want to call here
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+/*
+  after connecting to naming server we just need to pass microservice application name which
+  we want to connect it will automatically take url from eureka and also do load balancing
+*/
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
